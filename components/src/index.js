86b8ac5332e47f30314e6705a362d2803bca4edb
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-27 23:05:24 +0000

    parent to child w/ props for textComments & imgAvatars

--- components/src/index.js
+++ components/src/index.js
@@ -7,9 +7,24 @@ const App = () => {
   return (
     <div className="ui container comments">
       <br />
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        textComment="Awesome blog post!"
+        imgAvatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        textComment="This is so good."
+        imgAvatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 5:00PM"
+        textComment="So inspiring 😄"
+        imgAvatar={faker.image.avatar()}
+      />
     </div>
   );
 };
