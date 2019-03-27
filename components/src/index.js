eb24710215f84073f8470f58cb78ae2c9764ba56
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-27 23:38:15 +0000

    new ApprovalCard component w/ semantic-ui

--- components/src/index.js
+++ components/src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
       <br />
+      <ApprovalCard />
       <CommentDetail
         author="Sam"
         timeAgo="Today at 4:45PM"
