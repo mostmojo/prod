6ec54435188d0582e31dc0761c6b36264d6be354
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-27 12:47:15 +0000

    Differences between HTML & JSX. Using variable interpolation

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,14 +4,16 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 const App = () => {
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter a name";
   return (
     <div>
-      <label class="label" for="name">
-        Enter name:
+      <label className="label" for="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: "blue", color: "white" }}>
-        Submit
+        {buttonText.text}
       </button>
     </div>
   );
