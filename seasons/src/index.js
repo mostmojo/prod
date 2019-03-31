894037c7e62216c0845a1c2486038c6870780ef4
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-31 17:13:51 +0100

    Refactor in componentDidUpdate function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,20 +5,18 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     // this is the ONLY time we do direct assignment to this.state
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  // Always keep data loading material in componentDidMount(), not in constructor
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // we called setState!
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
+
   // React says we have to define render! (conditional rendering)
   render() {
     if (this.state.errorMessage && !this.state.lat) {
