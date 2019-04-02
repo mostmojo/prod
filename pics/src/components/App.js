728d6df42a4d3464c1dc402d5c7d6e22fdf405b5
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-04-02 15:21:17 +0100

    integrate axios to fetch data from unsplash api

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 7c1b6705c431388f9ab50f8ac5e72233a47f8f6d95f0309232fcd4f5e9073251"
+      }
+    });
   }
   render() {
     return (
