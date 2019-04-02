c044d8d64547ed1fbec52e150e378c12e089e4a8
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-04-02 23:31:29 +0100

    async await syntax for get request instead of promise syntax

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,14 +3,16 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization:
           "Client-ID 7c1b6705c431388f9ab50f8ac5e72233a47f8f6d95f0309232fcd4f5e9073251"
       }
     });
+
+    console.log(response.data.results);
   }
   render() {
     return (
