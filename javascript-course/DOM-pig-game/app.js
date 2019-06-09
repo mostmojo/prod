58665b44d8f4506db459621d8eed79d08114a66b
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-06-09 20:01:35 +0100

    Add dice roll event listener functionality

--- javascript-course/DOM-pig-game/app.js
+++ javascript-course/DOM-pig-game/app.js
@@ -15,18 +15,21 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-document.querySelector("#current-" + activePlayer).textContent = dice;
-// document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>"
-
-var x = document.querySelector("#score-0");
-console.log(x);
-
-// document.querySelector(".dice").style.display = "none";
+document.querySelector(".dice").style.display = "none";
 
 document.querySelector(".btn-roll").addEventListener('click', function() {
 // 1. Random number
 var dice = Math.floor(Math.random() * 6) + 1;
 // 2. Display result
-
+var diceDOM = document.querySelector(".dice");
+diceDOM.style.display = "block";
+diceDOM.src = "dice-" + dice + ".png";
 // 3. Update round score only if rolled number was not a 1.
 })
+
+
+
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>"
+// var x = document.querySelector("#score-0");
