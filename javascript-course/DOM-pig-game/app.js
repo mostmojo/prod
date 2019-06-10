a2a575496abd28644e3208fea1808747a36bef7a
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-06-10 23:57:31 +0100

    Update round score if number is between 2-6 and change player if it's 1 and make roundscore 0

--- javascript-course/DOM-pig-game/app.js
+++ javascript-course/DOM-pig-game/app.js
@@ -20,13 +20,22 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 
 document.querySelector(".btn-roll").addEventListener('click', function() {
-// 1. Random number
-var dice = Math.floor(Math.random() * 6) + 1;
-// 2. Display result
-var diceDOM = document.querySelector(".dice");
-diceDOM.style.display = "block";
-diceDOM.src = "dice-" + dice + ".png";
-// 3. Update round score only if rolled number was not a 1.
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // 2. Display result
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
+    // 3. Update round score only if rolled number was not a 1.
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    } else {
+        // Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+    }
 })
 
 
