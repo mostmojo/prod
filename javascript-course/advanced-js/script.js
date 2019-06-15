c39c7ef9648609ff4031be36d3f85a6c34100b77
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-06-15 19:07:20 +0100

    Create function to calc max heart rate to practice callbacks

--- javascript-course/advanced-js/script.js
+++ javascript-course/advanced-js/script.js
@@ -126,7 +126,7 @@ console.log(obj.city) // San Francisco (changes as it points to object)
 
 var years = [1990, 1965, 1937, 2005, 1998];
 
-//this func has 2 params. Loop and grab index and callback to 2nd function to calcAge
+// 1. this func has 2 params. Loop and grab index and callback to 2nd function to calcAge
 function arrayCalc(arr, fn) {
     var arrResult = [];
     for (var i = 0; i < arr.length; i++) {
@@ -135,11 +135,36 @@ function arrayCalc(arr, fn) {
     return arrResult;
 }
 
-// callback func which calcs age
+// 2. callback func which calcs age
 function calculateAge(element) {
-    return 2019 - element;
+    return 2016 - element;
 }
+// => 26, 51, 79, 11 , 18
+
+function isFullAge(element) {
+    return element >= 18;
+}
+// => t t t f t
+
+function maxHeartRate(element) {
+    if (element >= 18 && element <= 81) {
+        return Math.round(206.9 - (0.67 * element));
+    } else {
+        return -1;
+    }
+}
+// =>  [189, 173, 154, -1, 195]
+
 
 // var ages with arrayCalc functiion w/ 2 args where 2nd arg is a func
 var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate)
+
 console.log(ages);
+console.log(rates)
+
+/*
+1) create generic func that loops over an input array
+2) gave it a fn as input to calc something based on each element of the array
+*/
