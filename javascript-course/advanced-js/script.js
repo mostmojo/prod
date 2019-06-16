1ee8efb63d837aeef1fe6e20f9c9ef864631b28c
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-06-16 23:10:24 +0100

    Closures example

--- javascript-course/advanced-js/script.js
+++ javascript-course/advanced-js/script.js
@@ -233,10 +233,27 @@ function retirement(retirementAge) {
 }
 
 var retirementUS = retirement(66);
-retirementUS(1993);
+retirementUS(1993); // 40 years until retirement
 
-retirement(66)(1990);
+var retirementGermany = retirement(65);
+retirementGermany(1993) // 39 years until retirement
 
-// Result:
-// 40 years left until retirement.
-// (66 - (2019 - 1993))
+var retirementIceland = retirement(67);
+retirementIceland(1993) // 41 years until retirement
+
+// -------------------------------------------------
+
+
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach,' + name + '?');
+        } else {
+            console.log('Hello' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('John');
