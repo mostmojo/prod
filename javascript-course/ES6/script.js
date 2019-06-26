1a4f608208b922c7b55b1e6bbfe654a0096b11c6
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-06-26 10:02:35 +0100

    Add ES5 example of parent constructor

--- javascript-course/ES6/script.js
+++ javascript-course/ES6/script.js
@@ -422,6 +422,21 @@ for (let [key, value] of question.entries()) {
     }
 }
 
-const ans = parseInt(prompt('Write the correct answer'));
-console.log(question.get(ans === question.get('correct')));
+// const ans = parseInt(prompt('Write the correct answer'));
+// console.log(question.get(ans === question.get('correct')));
 
+// 🔸 --- Lecture: Classes --- 🔸
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+    var age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+}
+
+var john5 = new Person5('John', 1990, 'teacher');
