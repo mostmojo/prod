5d4bf0f7c20ca075cfeba4a41bbaa77dac71f523
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-06-26 13:15:13 +0100

    Add sub class Streets to challenge

--- javascript-course/ES6/script.js
+++ javascript-course/ES6/script.js
@@ -542,15 +542,36 @@ class Element {
     }
 }
 
-// Sub class
+// Sub class #1
 class Park extends Element {
     constructor(name, buildYear, area, numTrees) {
         super(name, buildYear);
-        this.area = area;
+        this.area = area; // km^2
         this.numTrees = numTrees;
     }
     // Method for calculating tree density of an object
     treeDensity() {
-        const density
+        const density = this.numTrees / this.area;
+        console.log(`${this.name} has a tree density of ${density} trees per square km.`);
+    }
+}
+
+// Sub class #2
+class Street extends Element {
+    constructor(name, buildYear, length, size = 3) {
+        super(name, buildYear);
+        this.length = length;
+        this.sizes = sizes;
+    }
+    // hash map
+    classifyStreet() {
+        const classification = new Map();
+        classification.set(1, 'tiny');
+        classification.set(2, 'small');
+        classification.set(3, 'normal');
+        classification.set(4, 'big');
+        classification.set(5, 'huge');
+        console.log(`${this.name}, built in ${this.buildYear}, is a ${classification.get(this.size)} street.`)
+
     }
 }
